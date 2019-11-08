cf3fa3d62a0414e07ebbe335ea63a65dcdd2ef09
h.motodera <h.motodera@jxpress.net>
2019-11-08 17:39:59 +0900

    Change write capacity

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -4,7 +4,7 @@
 resource "aws_dynamodb_table" "karma-dynamodb-table" {
   name           = "Karma"
   read_capacity  = "${var.karma_read_capacity}"
-  write_capacity = "${var.karma_read_capacity}"
+  write_capacity = "${var.karma_write_capacity}"
   hash_key       = "identifier"
 
   attribute {
